b904bc5bcd20560e23fa560e07517227fdc7b14e
keshanwen <seven.ke@yuanqu-tech.com>
2026-01-06 23:20:22 -0800

    feat: 测试同一个包，不版本的安装

--- apps/project3-react/src/App.tsx
+++ apps/project3-react/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { sum } from '@project-survey/utils' 
+import { sum as sum2 } from '@project-survey/utils2' 
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
   return (
     <>
       <div>
+        <h1>{sum(1,1)}</h1>
+         <h1>{sum2(1,1)}</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ packages/utils2/src/math.ts
@@ -0,0 +1,11 @@
+export function sum(a: number, b: number): number {
+  return (a + b) * 10;
+}
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
